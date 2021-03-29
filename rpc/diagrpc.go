a14b80f2b11afb17ad838f9418a3dfc1fb6e5a55
kubasiemion <kuba.siemion@gmail.com>
2021-03-29 16:47:15 +0200

    First HTTP state peek...

---
+++ rpc/diagrpc.go
@@ -0,0 +1,37 @@
+package rpc
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/san-lab/udpsigner/state"
+)
+
+func StartRPC(httpPort string, ctx context.Context, cancel context.CancelFunc, interruptChan chan os.Signal) {
+	fmt.Println("Starting http on port", httpPort)
+	//Beware! This config means that all the static images - also the ones called from the templates -
+	// have to be addressed as "/static/*", regardless of the location of the template
+	fs := http.FileServer(http.Dir("static"))
+	http.HandleFunc("/static/", http.StripPrefix("/"+
+		"static", fs).ServeHTTP)
+	http.HandleFunc("/", handleHttp)
+	srv := http.Server{Addr: "0.0.0.0:" + httpPort}
+	//This is to graciously serve the ^C signal - allow all registered routines to clean up
+	go func() {
+		select {
+		case <-interruptChan:
+			cancel()
+			srv.Shutdown(context.TODO())
+			return
+		}
+	}()
+
+	go srv.ListenAndServe()
+}
+
+func handleHttp(w http.ResponseWriter, r *http.Request) {
+	w.Write(state.CurrentState.DumpState())
+
+}
