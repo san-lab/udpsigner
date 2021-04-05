4c81d55a0c7898c80f41e0763d4ab7de7c405a03
kubasiemion <kuba.siemion@gmail.com>
2021-04-05 15:48:39 +0200

    templates

--- rpc/diagrpc.go
+++ rpc/diagrpc.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/san-lab/udpsigner/state"
+	"github.com/san-lab/udpsigner/templates"
 )
 
 func StartRPC(httpPort string, ctx context.Context, cancel context.CancelFunc, interruptChan chan os.Signal) {
@@ -14,8 +16,9 @@ func StartRPC(httpPort string, ctx context.Context, cancel context.CancelFunc, i
 	//Beware! This config means that all the static images - also the ones called from the templates -
 	// have to be addressed as "/static/*", regardless of the location of the template
 	fs := http.FileServer(http.Dir("static"))
-
+	renderer = templates.NewRenderer()
 	http.HandleFunc("/rpc", handleHttp)
+	http.HandleFunc("/react", serveHTML)
 	http.HandleFunc("/", fs.ServeHTTP)
 	srv := http.Server{Addr: "0.0.0.0:" + httpPort}
 	//This is to graciously serve the ^C signal - allow all registered routines to clean up
@@ -35,4 +38,20 @@ func handleHttp(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(state.CurrentState.DumpState())
 
+}
+
+var renderer = templates.NewRenderer()
+
+func serveHTML(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	isSlash := func(c rune) bool { return c == '/' }
+	f := strings.FieldsFunc(r.URL.Path, isSlash)
+	if len(f) < 3 {
+		fmt.Frintln(w, "Please, specify the template")
+		return
+
+	}
+	tempname := f[2]
+
+	w.Write([]byte("Templates here"))
 }
